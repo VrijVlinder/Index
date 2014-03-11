e1c58571e1eed34fa0807d9156f18ebb0cd1e1d9
VrijVlinder <adalib@mac.com>
2014-03-11 11:29:08 -0600

    New

---
+++ default.php
@@ -0,0 +1,62 @@
+<?php if (!defined('APPLICATION')) exit();
+
+
+// Define the plugin:
+
+
+$PluginInfo['Index'] = array(
+   'Name'=>'Index',
+   'Description' => "This plugin adds css dropdown menu to the menu.",
+   'Version' => '1.0',
+   'MobileFriendly'=>TRUE,
+   'Author' => "VrijVlinder",
+   'AuthorEmail' => 'contact@vrijvlinder.com',
+   'AuthorUrl' => "http://www.vrijvlinder.com"
+);
+
+class Index_Plugin extends Gdn_Plugin {
+
+  public function __construct() {
+      
+   }
+
+  
+  
+
+	
+   public function Setup() {
+		
+   }
+
+
+public function Base_Render_Before($Sender) {
+$Sender->AddJsFile('plugins/Index/index.js');
+$Sender->AddCssFile('plugins/Index/index.css');
+}
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     public function OnDisable() {
+      return TRUE;
+   }
+
+}
+
+
+	
+        	   
